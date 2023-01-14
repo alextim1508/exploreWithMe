8ea09dea00487fcc6a1ef1987b89d51889216601
Alex <atimofeev1508@gmail.com>
2023-01-14 15:47:53 +0300

    add stats db create script and dockers

--- stats/src/main/java/ru/practicum/explorewithme/statsServer/stats/model/Hits.java
+++ stats/src/main/java/ru/practicum/explorewithme/statsServer/stats/model/Hits.java
@@ -1,23 +1,32 @@
 package ru.practicum.explorewithme.statsServer.stats.model;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Entity
+@Table(name = "stats")
 public class Hits {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String app;
 
+    @Column(nullable = false)
     private String uri;
 
+    @Column(nullable = false)
     private String ip;
 
+    @CreationTimestamp
     private LocalDateTime created;
 }
